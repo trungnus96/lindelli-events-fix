7e17b299afd9594fd6d350fe0490f9c52fdbaaa0
trungnus96 <trungnus96@gmail.com>
2025-08-07 11:06:10 +1000

    Remove Google Tag Manager and gtag.js script tags from _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,47 +5,8 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          {/* Google tag (gtag.js) */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=AW-436267544"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'AW-436267544');
-              `,
-            }}
-          />
-          {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-KMSJC48');
-              `,
-            }}
-          />
-          {/* End Google Tag Manager */}
-        </Head>
+        <Head></Head>
         <body>
-          {/* Google Tag Manager (noscript) */}
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `
-                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMSJC48"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>
-              `,
-            }}
-          />
-          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
